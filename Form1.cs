9182695aac85c077cad93141b8b6d74157f1ec92
DIEGO CRUZ PATIÑO <143657851+DiegotsCodeHub@users.noreply.github.com>
2025-02-20 00:19:24 -0800

    Codigo Refactorizado para evitar gasto de recursos en recursividad

--- Form1.cs
+++ Form1.cs
@@ -9,49 +9,47 @@ namespace CruzPatinoDiego_Practica6_EventosdeBoton
         public FrmPrincipal()
         {
             InitializeComponent();
-
-         
             random = new Random();
             dynamicButtons = new List<Button>();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            MoveButton(btnpares, true);
-            MoveButton(btnimpares, false);
+            MoverBotones(btnpares, true);
+            MoverBotones(btnimpares, false);
         }
 
-        private void MoveButton(Button btn, bool isPar)
+        private void MoverBotones(Button btn, bool isPar)
         {
             int mayorX = this.ClientSize.Width - (btn.Width - 15);
             int mayorY = this.ClientSize.Height - (btn.Height - 15);
             int nuevoX = Random.Next(0, mayorX);
             int nuevoY = Random.Next(0, mayorY);
             btn.Location = new Point(nuevoX, nuevoY);
-            CheckCollisions(btn, isPar);
+            EventosColisiones(btn, isPar);
         }
 
-        private void CheckCollisions(Button btn, bool isPar)
+        private void EventosColisiones(Button btn, bool isPar)
         {
             if (btn.Top <= 0) // Toca la parte superior
                 this.BackColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
             if (btn.Bottom >= ClientSize.Height) // Toca la parte inferior
             {
-                if (isPar) GenerateButtons(5);
-                else RemoveButtons(3);
+                if (isPar) GenerarBotones(5);
+                else EliminarBotones(3);
             }
             if (btn.Left <= 0) // Toca la izquierda
             {
                 if (isPar) btn.BackColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
-                else ResetForm();
+                else ReiniciarForma();
             }
             if (btn.Right >= ClientSize.Width) // Toca la derecha
             {
-                if (!isPar) ChangeColorMultipleTimes(5);
+                if (!isPar) CambiodeColorMultiple(5);
             }
         }
 
-        private void GenerateButtons(int count)
+        private void GenerarBotones(int count)
         {
             for (int i = 0; i < count; i++)
             {
@@ -67,7 +65,7 @@ namespace CruzPatinoDiego_Practica6_EventosdeBoton
             }
         }
 
-        private void RemoveButtons(int count)
+        private void EliminarBotones(int count)
         {
             for (int i = 0; i < count && dynamicButtons.Any(); i++)
             {
@@ -76,7 +74,7 @@ namespace CruzPatinoDiego_Practica6_EventosdeBoton
             }
         }
 
-        private void ChangeColorMultipleTimes(int times)
+        private void CambiodeColorMultiple(int times)
         {
             for (int i = 0; i < times; i++)
             {
@@ -86,13 +84,11 @@ namespace CruzPatinoDiego_Practica6_EventosdeBoton
             }
         }
 
-        private void ResetForm()
+        private void ReiniciarForma()
         {
-            BackColor = DefaultBackColor;
-            btnpares.Location = new Point(50, 50);
-            btnimpares.Location = new Point(190, 50);
             foreach (var btn in dynamicButtons) Controls.Remove(btn);
             dynamicButtons.Clear();
+            Application.Restart();
         }
 
     }
